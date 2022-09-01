72aa10f738be8e5944573195099163b034bb09ce
Sean Oh <ohsean812@gmail.com>
2022-09-01 19:09:48 -0400

    commit tree df1a106f0e3ff7c161a874a59249da573b800f50

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    # validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :clickbait_phrase
+
+    def clickbait_phrase
+        phrase = [/Won't Believe/i, /Secret/i, /Top [number]/i, /Guess/i]
+        if phrase.none? { |start| start.match title }
+        errors.add(:title, "Must enter a catch phrase")        
+        end
+    end
+
 end
